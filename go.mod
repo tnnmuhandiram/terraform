9f7fc6543398ce9bc2285ce5ab19cd9ca1c08929
tharindu <tnnmuhandiram@gmail.com>
2019-10-24 09:50:51 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,55 @@
+// module github.com/gruntwork-io/terratest
+
+go 1.13
+
+require (
+	cloud.google.com/go v0.38.0
+	github.com/Azure/azure-sdk-for-go v32.5.0+incompatible
+	github.com/Azure/go-autorest/autorest v0.9.1
+	github.com/Azure/go-autorest/autorest/azure/auth v0.3.0
+	github.com/Azure/go-autorest/autorest/to v0.3.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.2.0 // indirect
+	github.com/aws/aws-sdk-go v1.23.8
+	github.com/docker/spdystream v0.0.0-20181023171402-6480d4af844c // indirect
+	github.com/elazarl/goproxy v0.0.0-20190911111923-ecfe977594f1 // indirect
+	github.com/ghodss/yaml v1.0.0
+	github.com/go-errors/errors v1.0.2-0.20180813162953-d98b870cc4e0 // indirect
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/uuid v0.0.0-20161128191214-064e2069ce9c
+	github.com/googleapis/gnostic v0.3.1 // indirect
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/gruntwork-io/gruntwork-cli v0.5.1
+	github.com/hashicorp/golang-lru v0.5.3 // indirect
+	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/jstemmer/go-junit-report v0.0.0-20190106144839-af01ea7f8024
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/magiconair/properties v1.8.1
+	github.com/mattn/go-zglob v0.0.2-0.20190814121620-e3c945676326 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/oracle/oci-go-sdk v7.1.0+incompatible
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/pquerna/otp v1.2.0
+	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/stretchr/testify v1.4.0
+	github.com/urfave/cli v1.21.0
+	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586
+	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	google.golang.org/api v0.9.1-0.20190821000710-329ecc3c9c34
+	google.golang.org/appengine v1.6.1 // indirect
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 // indirect
+	google.golang.org/grpc v1.23.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/api v0.0.0-20181110191121-a33c8200050f
+	k8s.io/apimachinery v0.0.0-20190704094520-6f131bee5e2c
+	k8s.io/client-go v0.0.0-20190704095228-386e588352a4
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	test_structure "./modules/test-structure"
+
+	"github.com/gruntwork-io/terratest/modules/gcp"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func main() {
+	TestTerraformGcpExample()
+}
+
+func TestTerraformGcpExample() {
+	fmt.Printf("=========================${t}")
+	t.Parallel()
+
+	exampleDir := test_structure.CopyTerraformFolderToTemp("./", "examples/terraform-gcp-example")
+
+	// Get the Project Id to use
+	projectId := gcp.GetGoogleProjectIDFromEnvVar(t)
+
+	// Create all resources in the following zone
+	zone := "us-east1-b"
+
+	// Give the example bucket a unique name so we can distinguish it from any other bucket in your GCP account
+	expectedBucketName := fmt.Sprintf("terratest-gcp-example-%s", strings.ToLower(random.UniqueId()))
+
+	// Also give the example instance a unique name
+	expectedInstanceName := fmt.Sprintf("terratest-gcp-example-%s", strings.ToLower(random.UniqueId()))
+
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: exampleDir,
+
+		// Variables to pass to our Terraform code using -var options
+		Vars: map[string]interface{}{
+			"gcp_project_id": projectId,
+			"zone":           zone,
+			"instance_name":  expectedInstanceName,
+			"bucket_name":    expectedBucketName,
+		},
+	}
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	// defer terraform.Destroy(t, terraformOptions)
+
+	// // This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	// terraform.InitAndApply(t, terraformOptions)
+
+	// // Run `terraform output` to get the value of some of the output variables
+	// bucketURL := terraform.Output(t, terraformOptions, "bucket_url")
+	// instanceName := terraform.Output(t, terraformOptions, "instance_id")
+
+	// // Verify that the new bucket url matches the expected url
+	// expectedURL := fmt.Sprintf("gs://%s", expectedBucketName)
+	// assert.Equal(t, expectedURL, bucketURL)
+
+	// // Verify that the Storage Bucket exists
+	// gcp.AssertStorageBucketExists(t, expectedBucketName)
+
+	// // Add a tag to the Compute Instance
+	// instance := gcp.FetchInstance(t, projectId, instanceName)
+	// instance.SetLabels(t, map[string]string{"testing": "testing-tag-value2"})
+
+	// // Check for the labels within a retry loop as it can sometimes take a while for the
+	// // changes to propagate.
+	// maxRetries := 12
+	// timeBetweenRetries := 5 * time.Second
+	// expectedText := "testing-tag-value2"
+
+	// retry.DoWithRetry(t, fmt.Sprintf("Checking Instance %s for labels", instanceName), maxRetries, timeBetweenRetries, func() (string, error) {
+	// 	// Look up the tags for the given Instance ID
+	// 	instance := gcp.FetchInstance(t, projectId, instanceName)
+	// 	instanceLabels := instance.GetLabels(t)
+
+	// 	testingTag, containsTestingTag := instanceLabels["testing"]
+	// 	actualText := strings.TrimSpace(testingTag)
+	// 	if !containsTestingTag {
+	// 		return "", fmt.Errorf("Expected the tag 'testing' to exist")
+	// 	}
+
+	// 	if actualText != expectedText {
+	// 		return "", fmt.Errorf("Expected GetLabelsForComputeInstanceE to return '%s' but got '%s'", expectedText, actualText)
+	// 	}
+
+	// 	return "", nil
+	// })
+}
